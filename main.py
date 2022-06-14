adffe9f15183494cace3824d7131d747092fc5e1
Maciej <kanabaj.maciej@gmail.com>
2022-06-15 01:38:06 +0200

    code modification to not keep connection

--- main.py
+++ main.py
@@ -10,19 +10,107 @@ from time import sleep
 from datetime import datetime
 
 class Email:
+
+    def __init__(self, uid : bytes):
+        self.mail_id = uid
+
+    def get_mail_header(self):
+        with EmailBox() as email_box:
+            email_box.select_folder('Inbox')
+            status, raw_content = email_box.connection.uid('fetch', self.mail_id, '(RFC822)')
+            if status == 'OK':
+                msg = email.message_from_bytes(raw_content[0][1])
+                #print(msg.keys())
+
+        return {
+            'status' : status,
+            'subject' : msg['Subject'],
+            'from' : msg['From']
+        }
+
+    def get_mail_content(self):
+        with EmailBox() as email_box:
+            email_box.select_folder('Inbox')
+            status, raw_content = email_box.connection.uid('fetch', self.mail_id, '(RFC822)')
+            if status == 'OK':
+                msg = email.message_from_bytes(raw_content[0][1])
+                if msg.is_multipart():
+                    msg_content = ''
+
+                    for part in msg.get_payload():
+                        if part.get_content_type() == 'text/plain':
+                            print(part)
+                            msg_content += part.get_payload()
+                        # if part.get_content_type() == 'text/html':
+
+                        #     msg_content += part.get_payload()
+                else:
+                    if msg.get_content_type() == 'text/html':
+                        msg_content = base64.b64decode(msg.get_payload()).decode('utf-8')
+
+        return msg_content
+
+    def mark_as_unseen(self):
+        with EmailBox() as email_box:
+            email_box.select_folder('Inbox')
+            status = email_box.connection.uid('STORE', self.mail_id, '-FLAGS', r'(\Seen)')
+            print(f'store {status}')
+            status = email_box.connection.expunge()
+            print(f'expunge {status}')
+
+    def move_email(self, folder : str) -> None:
+        with EmailBox() as email_box:
+            email_box.select_folder('Inbox')
+            status, _ = email_box.connection.uid('COPY', self.mail_id, folder)
+            print(f'coppy {status}')
+            if status == 'OK':
+                self.delete_email()
+
+    def delete_email(self):
+        with EmailBox() as email_box:
+            email_box.select_folder('Inbox')
+            status = email_box.connection.uid('STORE', self.mail_id , '+FLAGS', r'(\Deleted)')
+            print(f'store {status}')
+            status = email_box.connection.expunge()
+            print(f'expunge {status}')
+
+
+    def __repr__(self):
+        return str(self.mail_id.decode('utf-8'))
+
+class EmailBox:
     from config import domian, port, user, ssl_connection, password as _password
 
     def __init__(self) -> None:
-        self.connection = self.connect_ssl() if self.ssl_connection else self.connect()
+        self.connection = None
 
-    def connect(self):
+    def __enter__(self):
+        self.get_connection()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.disconect()
+
+    def get_connection(self):
+        try:
+            self.connection = self._connect_ssl() if self.ssl_connection else self._connect()
+        except ConnectionError as err:
+            raise err
+
+    def _connect(self):
         connection = imaplib.IMAP4(host=self.domian, port=self.port)
-        print(connection.login(self.user, self._password))
+        status = connection.login(self.user, self._password)
+        print(status)
+        if status[0] != 'OK':
+            raise ConnectionError(status[1])
         return connection
 
-    def connect_ssl(self):
+    def _connect_ssl(self):
         connection = imaplib.IMAP4_SSL(host=self.domian, port=self.port)
-        print(connection.login(self.user, self._password))
+        status = connection.login(self.user, self._password)
+        print(status)
+        if status[0] != 'OK':
+            raise ConnectionError(status[1])
         return connection
 
     def disconect(self):
@@ -31,57 +119,25 @@ class Email:
         status, _ = self.connection.logout()
         print(f'logout {status}')
 
-    def get_mails(self, folder = 'INBOX'):
+    def select_folder(self, folder):
+        print(self.connection.select(folder))
+
+    def get_mails(self, folder):
         try:
-            self.connection.select(folder)
+            self.select_folder(folder=folder)
             #status, index_list = self.connection.search(None, 'ALL')
             status, uids = self.connection.uid('search', None, 'ALL')
         except (imaplib.IMAP4.abort, imaplib.IMAP4_SSL.abort) as exc:
-            print('reconect', exc)
-            #self.disconect()
-            print(self.connection.logout())
-            self.__init__()
-            sleep(2)
+            self.get_connection()
             self.get_mails()
         else:
-            return status, uids[0].split()
+            emails = [Email(uid) for uid in uids[0].split()]
+            return status, emails
 
-    def get_mail_header(self, mail_id):
-        status, raw_content = self.connection.uid('fetch', mail_id, '(RFC822)')
-        if status == 'OK':
-            msg = email.message_from_bytes(raw_content[0][1])
-            #print(msg.keys())
 
-        return {
-            'status' : status,
-            'subject' : msg['Subject'],
-            'from' : msg['From']
-        }
-
-    def get_mail_content(self, mail_id):
-        status, raw_content = self.connection.uid('fetch',mail_id, '(RFC822)')
-        if status == 'OK':
-            msg = email.message_from_bytes(raw_content[0][1])
-            if msg.is_multipart():
-                msg_content = ''
-
-                for part in msg.get_payload():
-                    if part.get_content_type() == 'text/plain':
-                        print(part)
-                        msg_content += part.get_payload()
-                    # if part.get_content_type() == 'text/html':
-
-                    #     msg_content += part.get_payload()
-            else:
-                if msg.get_content_type() == 'text/html':
-                    msg_content = base64.b64decode(msg.get_payload()).decode('utf-8')
-
-        return msg_content
-
-class EmailListener(Email):
+class EmailListener:
 
     def __init__(self, freqency : int) -> None:
-        super().__init__()
         self.freq = freqency
         self.stop = False
         self.on_new_event = self._on_new_foo
@@ -89,18 +145,27 @@ class EmailListener(Email):
         self.events_list = []
 
     def run(self):
-        status, start_index_list = self.get_mails()
+        status = None
+        with EmailBox() as email_box:
+            status, emails = email_box.get_mails('Inbox')
+            if status == 'OK':
+                start_index_list = [mail.mail_id for mail in emails]
+
         if status == 'OK':
             try:
                 while not self.stop:
                     sleep_time = 60 // self.freq
-                    #print(sleep_time)
+                    print(sleep_time)
                     sleep(sleep_time)
-                    status , index_list = self.get_mails()
+                    with EmailBox() as email_box:
+                        status , emails = email_box.get_mails('Inbox')
+                    print(emails)
+                    index_list = [mail.mail_id for mail in emails]
                     if status != 'OK':
                         self.stop = True
-                    new_mails = list(set(index_list) - set(start_index_list))
-                    removed_mails = list(set(start_index_list) - set(index_list))
+
+                    new_mails = [Email(uid) for uid in (set(index_list) - set(start_index_list))]
+                    removed_mails = [Email(uid) for uid in (set(start_index_list) - set(index_list))]
                     current_time = datetime.now()
                     if new_mails:
                         self.on_new_event(event=new_mails, event_time = current_time)
@@ -109,9 +174,8 @@ class EmailListener(Email):
                         #print('new : ',\
                         #[(self.get_mail_header(id)['from'], self.get_mail_header(id)['subject']) for id in new_mails])
                     start_index_list = index_list
-                self.disconect()
             except KeyboardInterrupt:
-                self.disconect()
+                pass
 
     def _on_new_foo(self, event : list, event_time : datetime) -> None:
         self.events_list.append((f'new at {event_time}', event))
@@ -121,51 +185,26 @@ class EmailListener(Email):
 
 class EmailFilter(multiprocessing.Process):
 
-    process_list =[]
 
-    def __init__(self, mail_id):
+    def __init__(self, email : Email):
         super(EmailFilter, self).__init__()
-        self.mail_id = mail_id
+        self.email = email
         print('process init')
-        EmailFilter.process_list.append(self)
 
     def run(self):
         print('process run')
-        mail_box = Email()
-        mail_box.connection.select('INBOX')
-        mail_header = mail_box.get_mail_header(self.mail_id)
-        self.mark_as_unseen(mail_box)
+        mail_header = self.email.get_mail_header()
+        self.email.mark_as_unseen()
         if mail_header['subject'] == 'move':
             print('matching')
-            self.move_email(folder = 'TRASH', mail_box=mail_box)
-        mail_box.disconect()
-
-    def mark_as_unseen(self, mail_box : Email):
-        status = mail_box.connection.uid('STORE', self.mail_id, '-FLAGS', r'(\Seen)')
-        print(f'store {status}')
-        status = mail_box.connection.expunge()
-        print(f'expunge {status}')
-
-    def move_email(self, folder : str, mail_box : Email) -> None:
-        status, _ = mail_box.connection.uid('COPY', self.mail_id, folder)
-        print(f'coppy {status}')
-        if status == 'OK':
-            self.delete_email(mail_box)
-
-    def delete_email(self, mail_box : Email):
-        status = mail_box.connection.uid('STORE', self.mail_id , '+FLAGS', r'(\Deleted)')
-        print(f'store {status}')
-        status = mail_box.connection.expunge()
-        print(f'expunge {status}')
+            self.email.move_email(folder = 'Trash')
 
     def __del__(self):
         print('del ', self)
-        if not self.is_alive():
-            EmailFilter.process_list.remove(self)
 
 def filter_starter(event, event_time):
-    for mail_id in event:
-        email_filter = EmailFilter(mail_id)
+    for email in event:
+        email_filter = EmailFilter(email)
         email_filter.start()
 
 if __name__ == '__main__':
@@ -180,12 +219,12 @@ if __name__ == '__main__':
 
     #inbox.disconect()
     lst = EmailListener(freqency=4)
+
     #print(lst.connection.list())
     lst.on_new_event = filter_starter
     lst.on_remove_event = lambda event, event_time : print(f'at {event_time} removed {event}')
 
     lst.run()
-    print(lst.events_list)
-    for process in EmailFilter.process_list:
-        if process.is_alive():
+
+    for process in multiprocessing.active_children():
             process.join()
